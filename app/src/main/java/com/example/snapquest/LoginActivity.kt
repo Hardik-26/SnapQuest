0b7e3a1ee66769f5f3a1d5d2eb60027cdaefd0dd
Himanshi Agarwal <himanshiagarwal1408@gmail.com>
2024-10-01 08:31:18 +0530

    Splash Screen - Himanshi

---
+++ app/src/main/java/com/example/snapquest/LoginActivity.kt
@@ -0,0 +1,4 @@
+package com.example.snapquest
+
+class LoginActivity {
+}
--- app/src/main/java/com/example/snapquest/MainActivity.kt
+++ app/src/main/java/com/example/snapquest/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.snapquest
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var topAnimation : Animation
+    private lateinit var bottomAnimation : Animation
+    private lateinit var logo : CardView
+    private lateinit var app : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.splash)
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        logo = findViewById(R.id.logo)
+        app = findViewById(R.id.app_name)
+
+        logo.startAnimation(topAnimation)
+        app.startAnimation(bottomAnimation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
